48cbb907bd58cf14b8987b05d0670d73eaf6b8b7
Konstantin Roppel <k.roppel@epages.com>
2022-04-05 10:54:23 +0200

    Added test cases for double and char vectors

--- project/test/tests.cpp
+++ project/test/tests.cpp
@@ -14,3 +14,33 @@ TEST_CASE("Vector of type <int>", "[correctness]") {
     quicksort_parallelized(v, 0, v.size()-1);
     REQUIRE(v==w);
 }
+
+TEST_CASE("Vector of type <double>", "[correctness]") {
+    std::vector<int> v, w;
+    double values[5] {1.67, 2.38, 0.4, 1.11, 5.9};
+    double sorted_values[5] {0.4, 1.11, 1.67, 2.38, 5.9};
+
+    for (const int value:values) {
+        v.push_back(value);
+    }
+    for (const int value:sorted_values) {
+        w.push_back(value);
+    }
+    quicksort_parallelized(v, 0, v.size()-1);
+    REQUIRE(v==w);
+}
+
+TEST_CASE("Vector of type <Char>", "[correctness]") {
+    std::vector<int> v, w;
+    char values[5] {'e', 'x', 'a', 'd', 'h'};
+    char sorted_values[5] {'a', 'd', 'e', 'h', 'x'};
+
+    for (const int value:values) {
+        v.push_back(value);
+    }
+    for (const int value:sorted_values) {
+        w.push_back(value);
+    }
+    quicksort_parallelized(v, 0, v.size()-1);
+    REQUIRE(v==w);
+}
