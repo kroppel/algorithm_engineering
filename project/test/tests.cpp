e0f26440eb9bd7764244f30d063767c2c328d78b
Konstantin Roppel <k.roppel@epages.com>
2022-04-10 16:09:59 +0200

    Added tests for partitioning

--- project/test/tests.cpp
+++ project/test/tests.cpp
@@ -4,10 +4,8 @@
 #include <iomanip>
 #include <algorithm>
 
-using namespace std;
-
 TEST_CASE("Partition_fetch_add: vector of type <int> with 6 elements", "[correctness]") {
-    vector<int> v1, v2;
+    std::vector<int> v1, v2, v3;
     int values[6] = {5,6,3,2,7,1};
 
     for (const int value:values) {
@@ -15,47 +13,58 @@ TEST_CASE("Partition_fetch_add: vector of type <int> with 6 elements", "[correct
     }
 
     v2 = v1;
+    v3 = v1;
     int pivot = v1.back();
-    cout << "Pivot: " << pivot << "\n";
 
     partition(v1.begin(), v1.end(), [&pivot](int x) {return x<=pivot;});
+    //partition_fetch_add(v2, v2.size(), v2.size()-1);
+    partition_fetch_add(v2.begin(), v2.end(), [&pivot](int x) {return x<=pivot;});
+    partition_pivot(v3, 0, v3.size()-1, pivot);
 
-    partition_fetch_add(v2, v2.size(), pivot);
+    //REQUIRE(v1 == v2);
+    REQUIRE(std::is_partitioned(v2.begin(), v2.end(), [&pivot](int x) {return x<=pivot;}));
+    REQUIRE(v1 == v3);
 
-    REQUIRE(v1 == v2);
 }
 
-TEST_CASE("Partition_fetch_add: vector of type <int> with 1000000 elements", "[correctness]") {
-    double start_time, runtime1, runtime2;
-    int p1, p2;
-    const size_t size = 10;
-    vector<int> v1, v2;
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> d(0, size);
+TEST_CASE("Partition_fetch_add: vector of type <int> with 10000000 elements", "[correctness]") {
+    double start_time, runtime1, runtime2, runtime3;
+    const size_t size = 100000000;
+    std::vector<int> v1, v2, v3;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> d(0, size);
     // Fill vectors with random numbers between 0 and size - 1
-    cout << "Create vectors with " << size << " random elements.\n";
+    std::cout << "Create vectors with " << size << " random elements.\n";
     for (int i = 0; i < size; ++i) {
         v1.push_back(d(gen));
     }
 
     v2 = v1;
+    v3 = v1;
+
     int pivot = v1.at(size-1);
-    cout << "Pivot: " << pivot << "\n";
 
     start_time = omp_get_wtime();
     partition(v1.begin(), v1.end(), [&pivot](int x) {return x<=pivot;});
     runtime1 = omp_get_wtime() - start_time;
 
     start_time = omp_get_wtime();
-    partition_fetch_add(v2, v2.size(), pivot);
+    partition_fetch_add(v2, v2.size(), v2.size()-1);
     runtime2 = omp_get_wtime() - start_time;
 
-    REQUIRE(v1 == v2);
+    start_time = omp_get_wtime();
+    partition_pivot(v3, 0, v3.size()-1, pivot);
+    runtime3 = omp_get_wtime() - start_time;
 
-    cout << "std::partition with runtime: " << setprecision(6) << runtime1 << "s\n";
-    cout << "partition_fetch_add with runtime: " << setprecision(6) << runtime2 << "s\n";
-    cout << "and MINIMUM_VECTOR_ELEMENT_NUMBER=" << MINIMUM_VECTOR_ELEMENT_NUMBER << "\n";
+    REQUIRE(std::is_partitioned(v2.begin(), v2.end(), [&pivot](int x) {return x<=pivot;}));
+    REQUIRE(std::is_partitioned(v3.begin(), v3.end(), [&pivot](int x) {return x<=pivot;}));
+
+
+    std::cout << "std::partition with runtime: " << std::setprecision(6) << runtime1 << "s\n";
+    std::cout << "partition_fetch_add with runtime: " << std::setprecision(6) << runtime2 << "s\n";
+    std::cout << "partition_pivot with runtime: " << std::setprecision(6) << runtime3 << "s\n";
+    std::cout << "and MINIMUM_VECTOR_ELEMENT_NUMBER=" << MINIMUM_VECTOR_ELEMENT_NUMBER << "\n";
 }
 
 TEST_CASE("Quicksort: vector of type <int>", "[correctness]") {
